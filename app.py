80343c06410f6c9f25eb47f0f6d46e19bd5d3b59
devanshi-narula <worknarula@gmail.com>
2025-04-09 16:58:12 +0530

    updated app

--- app.py
+++ app.py
@@ -61,5 +61,6 @@ def index():
 
     return render_template('index.html', result=result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
